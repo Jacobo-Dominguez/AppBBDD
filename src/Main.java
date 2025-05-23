d253e7d0acb8d962ff53e6a932e22af339933cbc
Jacobo-Dominguez <jacobo9810@gmail.com>
2025-05-23 15:55:25 +0200

    Configuracion de proyecto

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import vistas.VentanaLogin;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new VentanaLogin());
+
+    }
+}
